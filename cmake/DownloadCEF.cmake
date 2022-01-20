fa3995eaa074f13b71e409b1559d9844444f040a
Senthil Kumar <supersen@gmail.com>
2022-01-19 23:52:39 -0800

    CEF_Root fix

--- cmake/DownloadCEF.cmake
+++ cmake/DownloadCEF.cmake
@@ -42,7 +42,7 @@ function(DownloadCEF platform version escaped_version download_dir)
     # Extract the binary distribution.
     message(STATUS "Extracting ${CEF_DOWNLOAD_PATH}...")
     execute_process(
-      COMMAND ${CMAKE_COMMAND} -E tar xzf "${CEF_DOWNLOAD_DIR}//${CEF_DISTRIBUTION}/${CEF_DOWNLOAD_FILENAME}"
+      COMMAND ${CMAKE_COMMAND} -E tar xzf "${CEF_DOWNLOAD_DIR}/${CEF_DISTRIBUTION}/${CEF_DOWNLOAD_FILENAME}"
       WORKING_DIRECTORY ${CEF_DOWNLOAD_DIR}
       )
   endif()
