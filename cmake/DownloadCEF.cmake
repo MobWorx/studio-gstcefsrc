dbebe34afe5145bf0a7a46db4bea3eadd041e01b
Senthil Kumar <supersen@gmail.com>
2022-01-19 16:29:39 -0800

    had disable

--- cmake/DownloadCEF.cmake
+++ cmake/DownloadCEF.cmake
@@ -34,7 +34,7 @@ function(DownloadCEF platform version escaped_version download_dir)
       message(STATUS "Downloading ${CEF_DOWNLOAD_PATH}...")
       file(
         DOWNLOAD "${CEF_DOWNLOAD_URL}" "${CEF_DOWNLOAD_PATH}"
-        EXPECTED_HASH SHA1=${CEF_SHA1}
+        #EXPECTED_HASH SHA1=${CEF_SHA1}
         SHOW_PROGRESS
         )
     endif()
