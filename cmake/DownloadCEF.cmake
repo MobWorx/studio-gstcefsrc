bd196f41f3556dec714c8424aa7bcdb8e9c6c400
Senthil Kumar <supersen@gmail.com>
2022-01-20 09:18:18 -0800

    path fix

--- cmake/DownloadCEF.cmake
+++ cmake/DownloadCEF.cmake
@@ -45,5 +45,17 @@ function(DownloadCEF platform version escaped_version download_dir)
       COMMAND ${CMAKE_COMMAND} -E tar -xzf "${CEF_DOWNLOAD_DIR}/${CEF_DOWNLOAD_FILENAME}"
       WORKING_DIRECTORY ${CEF_DOWNLOAD_DIR}
       )
+      SUBDIRLIST(SUBDIRS ${CEF_DOWNLOAD_DIR})
   endif()
 endfunction()
+
+MACRO(SUBDIRLIST result curdir)
+  FILE(GLOB children RELATIVE ${curdir} ${curdir}/*)
+  SET(dirlist "")
+  FOREACH(child ${children})
+    IF(IS_DIRECTORY ${curdir}/${child})
+      LIST(APPEND dirlist ${child})
+    ENDIF()
+  ENDFOREACH()
+  SET(${result} ${dirlist})
+ENDMACRO()
